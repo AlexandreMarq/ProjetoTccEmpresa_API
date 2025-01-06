c283f78d58aec70c2d742189e658d5da362b8257
Alexandre Marques <alexandre.marques@unidas.com.br>
2025-01-06 10:56:34 -0300

    new: project

---
+++ ProjetoTccEmpresa_API/01 - Controllers/TccEmpresaController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoTccEmpresa_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TccEmpresaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TccEmpresaController> _logger;
+
+        public TccEmpresaController(ILogger<TccEmpresaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
